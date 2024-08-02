d34276214ede6a07e0b4b8a7b85518e1ae547e65
lyhao223 <lyhao0710@gmail.com>
2024-08-02 16:53:23 +0700

    update reponsive order detail

--- src/app/UI/Component/Orders/Orders.tsx
+++ src/app/UI/Component/Orders/Orders.tsx
@@ -1,7 +1,14 @@
-"use client"
-import { Accordion, AccordionDetails, AccordionSummary, Pagination, Stack } from '@mui/material'
-import { useSession } from 'next-auth/react';
-import React, { use, useEffect, useState } from 'react'
+"use client";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Pagination,
+  Stack,
+} from "@mui/material";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React, { use, useEffect, useState } from "react";
 import { FaArrowDown } from "react-icons/fa";
 interface Item {
   id: string;
@@ -18,78 +25,93 @@ interface Order {
   paymentMethod: string;
 }
 const Orders = () => {
-    const [orders, setOrders] = useState<Order[]>([]);
-    const [currentPage, setCurrentPage] = React.useState(1);
-    const { data: session, status } = useSession();
-    const fetchOrders = async() => {
-        try {
-           const res = await fetch('/api/checkout', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-            if(res.ok) {
-                const data = await res.json()
-                setOrders(data.orders)
-                console.log(orders)
-            }else{
-                console.error('Failed to fetch orders')
-            }
-        } catch (error) {
-            console.error('Failed to fetch orders')
-        }
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const { data: session, status } = useSession();
+  const fetchOrders = async () => {
+    try {
+      const res = await fetch("/api/checkout", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setOrders(data.orders);
+        console.log(orders);
+      } else {
+        console.error("Failed to fetch orders");
+      }
+    } catch (error) {
+      console.error("Failed to fetch orders");
     }
+  };
 
-    useEffect(() => {
-        if(status === 'authenticated') {
-            fetchOrders()
-        }
-    }, [status])
+  useEffect(() => {
+    if (status === "authenticated") {
+      fetchOrders();
+    }
+  }, [status]);
 
-    const orderPerPages = 4;
-    const startIndex = (currentPage - 1) * orderPerPages;    
-    const paginatedOrder = orders.slice(startIndex, startIndex + orderPerPages);
-    const totalPages = Math.ceil(orders.length / orderPerPages);
-    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        setCurrentPage(value);
-    };
+  const orderPerPages = 4;
+  const startIndex = (currentPage - 1) * orderPerPages;
+  const paginatedOrder = orders.slice(startIndex, startIndex + orderPerPages);
+  const totalPages = Math.ceil(orders.length / orderPerPages);
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setCurrentPage(value);
+  };
   return (
-    <div className='flex flex-col items-start justify-start space-y-5'>
-      <div className='flex items-start justify-start'>
-        <h1 className='text-4xl font-semibold tracking-widest subpixel-antialiased'>Your Orders</h1>
-      </div>
-      <div className='flex flex-col items-center justify-center space-y-4'>
-      {orders.length > 0 ? (
-          paginatedOrder.map((order, index) => (
-            <Accordion key={order.orderID} className="w-[40rem]">
-              <AccordionSummary expandIcon={<FaArrowDown />}>
-                <h1>Order {startIndex + index + 1}</h1>
-              </AccordionSummary>
-              <AccordionDetails>
-                <div className="flex flex-col items-start justify-start space-y-2">
-                  <h1>Order ID: {order.orderID}</h1>
-                  {/* Add other details as needed */}
-                  {order.items.map((item, itemIndex) => (
-                    <div key={itemIndex}>
-                      <h2>Item {itemIndex + 1}: {item.title}</h2>
-                      <p>Quantity: {item.quantity}</p>
-                      <p>Price: ${item.price}</p>
-                      <img src={item.image} alt={item.title} width="50" />
-                    </div>
-                  ))}
-                  <h1>Total Amount: ${order.totalAmount.toFixed(2)}</h1>
-                  <h1>Payment Method: {order.paymentMethod}</h1>
-                </div>
-              </AccordionDetails>
-            </Accordion>
-          ))
-        ) : (
-          <p>No orders found.</p>
-        )}
-      </div>
+    <div className="flex flex-col items-start justify-start space-y-5">
+      <h1 className="text-xl xl:text-4xl 2xl:text-4xl lg:text-3xl font-semibold tracking-widest subpixel-antialiased">
+        Your Orders
+      </h1>
+      <div className="flex flex-col items-center justify-center 2xl:mx-0 xl:mx-60 lg:mx-10 md:mx-7 mx-0">
+        <div className="flex flex-col items-start justify-start 2xl:items-center 2xl:justify-center xl:items-center xl:justify-center lg:items-center lg:justify-center space-y-4">
+          {orders.length > 0 ? (
+            paginatedOrder.map((order, index) => (
+              <Accordion
+                key={order.orderID}
+                className="w-80 2xl:w-[40rem] xl:w-[40rem] lg:w-[40rem] md:w-[36rem]">
+                <AccordionSummary expandIcon={<FaArrowDown />}>
+                  <h1 className="subpixel-antialiased text-lg font-medium tracking-wider">
+                    Order {startIndex + index + 1}
+                  </h1>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <div className="flex flex-col items-start justify-start space-y-4">
+                    <h1 className="text-sm 2xl:text-lg xl:text-lg lg:text-lg md:text-lg font-semibold text-red-600 tracking-tight">
+                      Order ID: {order.orderID.toUpperCase()}
+                    </h1>
+                    {/* Add other details as needed */}
+                    {order.items.map((item, itemIndex) => (
+                      <Link
+                        href={`/shop/productID/${item.id}`}
+                        key={itemIndex}
+                        className="flex flex-row items-center justify-center space-x-10 border px-3 rounded-lg shadow-lg 2xl:w-[38rem] 2xl:h-32 xl:w-[38rem] xl:h-32 lg:w-[38rem] lg:h-32 md:w-full md:h-32  text-sm">
+                        <h2 className="text-nowrap hidden xl:block 2xl:block lg:block md:block">
+                          Item {itemIndex + 1}
+                        </h2>
+                        <img src={item.image} alt={item.title} width="50" />
+                        <h2 className="">{item.title}</h2>
+                        <p className="text-nowrap ">
+                          Quantity: {item.quantity}
+                        </p>
+                        <p className="text-nowrap">Price: ${item.price}</p>
+                      </Link>
+                    ))}
+                    <h1>Total Amount: ${order.totalAmount.toFixed(2)}</h1>
+                    <h1>Payment Method: {order.paymentMethod}</h1>
+                  </div>
+                </AccordionDetails>
+              </Accordion>
+            ))
+          ) : (
+            <p>No orders found.</p>
+          )}
+        </div>
 
-      <div className="flex items-center justify-center my-12">
+        <div className="flex items-center justify-center my-12">
           <Stack>
             <Pagination
               count={totalPages}
@@ -100,8 +122,9 @@ const Orders = () => {
             />
           </Stack>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
--- src/app/accountdetail/orders/page.tsx
+++ src/app/accountdetail/orders/page.tsx
@@ -1,12 +1,12 @@
-
-import Orders from '@/app/UI/Component/Orders/Orders'
-import React from 'react'
+import Orders from "@/app/UI/Component/Orders/Orders";
+import React from "react";
 
 const Page = () => {
-
   return (
-    <div className='p-32 w-full'><Orders /></div>
-  )
-}
+    <div className="p-10 2xl:p-32 xl:p-32 lg:p-32 w-full">
+      <Orders />
+    </div>
+  );
+};
 
-export default Page
+export default Page;
